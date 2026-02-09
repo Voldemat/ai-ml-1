679a8f9a36d1b2c0b26378e9b9af4bb6d15addc4
Vladimir Voitenko <vladimirdev635@gmail.com>
2026-02-09 12:12:28 +0500

    update

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -5,13 +5,9 @@ rng = np.random.default_rng(1)
 
 x = rng.integers(low=1, high=1000, size=10).reshape(-1, 1)
 y = rng.integers(low=1, high=1000, size=10).reshape(-1, 1)
+print(x, y)
 
 model = LinearRegression().fit(x, y)
 
 print("Slope (price per m2): ", model.coef_[0])
 print("Base price: ", model.intercept_)
-
-new_area = 90
-predicted_price = model.predict([[new_area]])
-
-print("Predicted price for", new_area, "m²:", predicted_price[0])
